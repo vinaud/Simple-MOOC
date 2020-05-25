a4eef1bd389fe306d6e75c699f1b08816fcb60dd
João Vinaud <leyenasd@gmail.com>
2020-05-25 11:22:11 -0300

    configuração de url

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from simplemooc.core import views
 
 urlpatterns = [
+    path(r'^S', 'simplemooc.core.views.home', name='home'),
     path('admin/', admin.site.urls),
 ]

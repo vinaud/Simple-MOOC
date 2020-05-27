0cc7c48ff2b784b7aef213836ba789aa200b3ff9
João Vinaud <leyenasd@gmail.com>
2020-05-27 10:43:29 -0300

    Primeiro template

--- simplemooc/core/views.py
+++ simplemooc/core/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<div>Hello Warudo<div/>')
+    return render(request, 'home.html', {'usuario': 'Vinaud'})

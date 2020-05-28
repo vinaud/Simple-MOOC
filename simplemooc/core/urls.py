7c268daf1c346fda9ba69b7d98d7da69a8efef82
João Vinaud <leyenasd@gmail.com>
2020-05-28 11:45:42 -0300

    configuração de namespace

--- simplemooc/core/urls.py
+++ simplemooc/core/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from simplemooc.core.views import home
 from simplemooc.core.views import contact
 
+app_name="core"
+
 urlpatterns = [
     path('', home, name='home'),
     path('contato/', contact, name='contact')
--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import include, path
 from simplemooc.core import urls
 
+
+
 urlpatterns = [
-    path('', include('simplemooc.core.urls')),
+    path('', include('simplemooc.core.urls', namespace = 'core')),
     path('admin/', admin.site.urls)
 ]

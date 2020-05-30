da2f191ae2431a5edc94acd774735b297bb3bf9e
João Vinaud <leyenasd@gmail.com>
2020-05-30 15:46:52 -0300

    alteração no modelo

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -6,6 +6,6 @@ class Course(models.Model):
     slug = models.SlugField('Atalho')
     description = models.TextField('Descrição', blank=True)
     start_date = models.DateField('Data de início', null=True, blank=True)
-    image = models.ImageField(upload_to='courses/images', verbose_name='Imagem')
+    image = models.ImageField(upload_to='courses/images', verbose_name='Imagem', null=True, blank=True)
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
     updated_at = models.DateTimeField('Atualizado em', auto_now_add=True)

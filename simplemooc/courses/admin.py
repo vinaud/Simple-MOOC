863419480dfe5aa50cac288fb84c7ca22dc8f633
João Vinaud <leyenasd@gmail.com>
2020-06-12 13:05:11 -0300

    personalização dos campos visualizados no admin

--- simplemooc/courses/admin.py
+++ simplemooc/courses/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 
 from .models import Course
-admin.site.register(Course)
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display=['name', 'slug', 'start_date', 'created_at']
+    search_fields=['name', 'slug']
+
+admin.site.register(Course, CourseAdmin)

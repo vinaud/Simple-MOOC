bcaec0b794be733a059636d94290035f09a6856a
João Vinaud <leyenasd@gmail.com>
2020-06-15 09:14:26 -0300

    correção

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def courses(request):
     template_name = 'course/courses.html'
-    return render(request,tempalte_name)
+    return render(request,template_name)

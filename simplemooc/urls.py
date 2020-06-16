6c771097d5ebad1dd89b12202e3b102a6e322f33
João Vinaud <leyenasd@gmail.com>
2020-06-16 10:24:44 -0300

    Visualização das imagens em modo debug

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from simplemooc.core import urls
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -24,3 +25,6 @@ urlpatterns = [
     path('cursos/', include('simplemooc.courses.urls', namespace = 'courses')),
     path('admin/', admin.site.urls)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

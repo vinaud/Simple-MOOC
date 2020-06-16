a039f3d2e27357aa677f67cb6eb81a48a20b4f7d
João Vinaud <leyenasd@gmail.com>
2020-06-16 09:55:52 -0300

    Consulta e listagem dos cursos cadastrados no banco

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Course
 
 def index(request):
+    courses = Course.objects.all()
     template_name = 'courses/index.html'
-    return render(request,template_name)
+    context = {
+        'courses': courses
+    }
+    return render(request, template_name, context)

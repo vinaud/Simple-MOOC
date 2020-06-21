5603688d68d072dd94fedb20c9d6d50f4d517e03
João Vinaud <leyenasd@gmail.com>
2020-06-21 11:12:02 -0300

    Configuração do serviço de email

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -124,3 +124,11 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
 
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'Nome <email@gmail.com>'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_PASSORD = 'senha'
+EMAIL_PORT = 587

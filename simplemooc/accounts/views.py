b4fbe5219063ba68e472ac37683d30426f4f3359
João Vinaud <leyenasd@gmail.com>
2020-06-27 11:34:14 -0300

    Redirecionamento de usuário após cadastro

--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.conf import settings
 
 def register(request):
     template_name = 'accounts/register.html'
-    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user = authenticate(
+                username=user.username, password=form.cleaned_data['password1']
+            )
+            login(request, user)
+            return redirect('core:home')
+    else:
+        form = UserCreationForm()
     context = {
-        'form': UserCreationForm()
+        'form': form
     }
     return render(request, template_name, context)

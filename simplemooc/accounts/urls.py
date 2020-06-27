8e9014184bcf05e4929d0d65014af3ca34a1b4c2
João Vinaud <leyenasd@gmail.com>
2020-06-27 11:27:52 -0300

    Configuração básica do a cesso a página de cadastro de usuário

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, re_path
 from django.contrib.auth.views import LoginView
+from simplemooc.accounts.views import register
 
 
 app_name="accounts"
 
 urlpatterns = [
     re_path(r'^entrar/$', LoginView.as_view(), {'template_name': 'accounts/login.html'},  name='login'),
-    #r'^entrar/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}, name='login'
-    
-   
+    re_path(r'^cadastre-se/$', register,  name='register'),
+
 ]
--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def register(request):
+    template_name = 'accounts/register.html'
+    
+    context = {
+        'form': UserCreationForm()
+    }
+    return render(request, template_name, context)

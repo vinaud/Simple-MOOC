d1d5f34732b1f6e0f4e506b04f1932ac9e906a9d
João Vinaud <leyenasd@gmail.com>
2020-07-14 10:00:09 -0300

    campo de email no formulario de cadastro

--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -4,10 +4,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.conf import settings
 
+from .forms import RegisterForm
+
 def register(request):
     template_name = 'accounts/register.html'
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             user = authenticate(

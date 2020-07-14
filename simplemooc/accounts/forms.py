33cb9559df35dd537241908d40fff9f952bf630a
João Vinaud <leyenasd@gmail.com>
2020-07-14 10:17:35 -0300

    metodo para cadastrar o usuário do form

--- simplemooc/accounts/forms.py
+++ simplemooc/accounts/forms.py
@@ -4,3 +4,10 @@ from django.contrib.auth.forms import UserCreationForm
 class RegisterForm(UserCreationForm):
 
     email = forms.EmailField(label = "E-mail")
+
+    def save(self, commit = True):
+        user = super(RegisterForm, self).save(commit = False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user

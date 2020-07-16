7fce964c96714ed04f620f51b3c31143e44fe832
João Vinaud <leyenasd@gmail.com>
2020-07-16 15:11:05 -0300

    logout

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, re_path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView 
+from django.contrib.auth import views 
+
 from simplemooc.accounts.views import register
 
 
@@ -7,6 +9,7 @@ app_name="accounts"
 
 urlpatterns = [
     re_path(r'^entrar/$', LoginView.as_view(), {'template_name': 'accounts/login.html'},  name='login'),
+    re_path(r'^sair/$', views.logout, {'next_page': 'core:home'},  name='logout'),
     re_path(r'^cadastre-se/$', register,  name='register'),
 
 ]

5774e354a1395f6f3e1467beb3a0bfb2c9b8e6c5
João Vinaud <leyenasd@gmail.com>
2020-07-24 10:39:46 -0300

    Alteração de senha do usuário

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth.views import LogoutView
 from simplemooc.accounts.views import register
 from simplemooc.accounts.views import dashboard 
 from simplemooc.accounts.views import edit
+from simplemooc.accounts.views import edit_password
 
 
 app_name="accounts"
@@ -15,5 +16,6 @@ urlpatterns = [
     re_path(r'^sair/$', LogoutView.as_view(), {'next_page': 'core:home'},  name='logout'),
     re_path(r'^cadastre-se/$', register,  name='register'),
     re_path(r'^editar/$', edit,  name='edit'), 
+    re_path(r'^editar-senha/$', edit_password,  name='edit_password'), 
 
 ]
--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.conf import settings
@@ -41,5 +41,21 @@ def edit_password(request):
             context['success'] = True
     else:
         form = PasswordChangeForm(user=request.user)
+    context['form'] = form
+    return render(request, template_name, context)
+
+
+@login_required
+def edit(request):
+    template_name = 'accounts/edit.html'
+    context = {}
+    if request.method == 'POST':
+        form = EditAccountForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            form = EditAccountForm(instance=request.user)
+            context['success'] = True
+    else:
+        form = EditAccountForm(instance=request.user)
     context['form'] = form
     return render(request, template_name, context)

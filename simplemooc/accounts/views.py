3dd8dc9a7e7264fd50d7de47b57502c3db94a1a2
João Vinaud <leyenasd@gmail.com>
2020-07-28 10:18:23 -0300

    Custom model de usuário

--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login

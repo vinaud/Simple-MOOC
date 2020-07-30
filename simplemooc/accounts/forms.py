f18b62c62d147afa4adc83fbcc8b21569b6198f7
João Vinaud <leyenasd@gmail.com>
2020-07-30 10:36:00 -0300

    Ajustes no custom User

--- simplemooc/accounts/forms.py
+++ simplemooc/accounts/forms.py
@@ -1,32 +1,36 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
-class RegisterForm(UserCreationForm):
+User = get_user_model()
 
-    email = forms.EmailField(label = "E-mail")
+class RegisterForm(forms.ModelForm):
+    
+    password1 = forms.CharField(label='Senha', widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='Confirmação de Senha', widget=forms.PasswordInput
+    )
 
-    def clean_email(self):
-        email = self.cleaned_data['email']
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("Este e-mail já está cadastrado")
-        return email
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('A confirmação não está correta')
+        return password2
 
-
-    def save(self, commit = True):
-        user = super(RegisterForm, self).save(commit = False)
-        user.email = self.cleaned_data['email']
+    def save(self, commit=True):
+        user = super(RegisterForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
         if commit:
             user.save()
         return user
 
-class EditAccountForm(forms.ModelForm):
-    def clean_email(self):
-        email = self.cleaned_data['email']
-        queryset = User.objects.filter(email=email).exclude(pk = self.instance.pk)
-        if queryset.exists():
-            raise forms.ValidationError("Este e-mail já está cadastrado")
-        return email
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name']
+        fields = ['username', 'email']
+
+class EditAccountForm(forms.ModelForm):
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'name']
